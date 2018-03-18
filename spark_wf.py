d0c7b5f0e626bc4ca4b62152c355d8005d3f936b
valhayot <valeriehayot@gmail.com>
2018-03-18 22:29:10 +0000

    completed single t1 anatomical template wf

--- spark_wf.py
+++ spark_wf.py
@@ -9,19 +9,20 @@ from fmriprep.interfaces import(
         FSDetectInputs, NormalizeSurf, GiftiNameSource, TemplateDimensions, Conform, Reorient,
         ConcatAffines, RefineBrainMask,
 )
+from fmriprep.utils.misc import add_suffix
 from pkg_resources import resource_filename as pkgr
 import os, bunch, socket
 
 
-# Global variables to eventually be adapted to command-line tool
+# Global variables to eventually be adapted for command-line tool
 workflow_name = 'test_anat_template'
 input_t1w = [os.path.abspath('data/ds052/sub-01/anat/sub-01_run-01_T1w.nii.gz'),
         os.path.abspath('data/ds052/sub-01/anat/sub-01_run-02_T1w.nii.gz')]
 longitudinal=False
-num_t1w=2
+num_t1w=1
 workdir = os.getcwd() + '/' +  workflow_name
 
-def runTemplateDimensions(part):
+def templateDimensionsTask(part):
     in_img = list(part)
 
 
@@ -45,17 +46,18 @@ def runTemplateDimensions(part):
    
     target_zooms = [td._results['target_zooms']] * len(td._results['t1w_valid_list'])
     target_shapes = [td._results['target_shape']] * len(td._results['t1w_valid_list'])
+    out_report = [td._results['out_report']] * len(td._results['t1w_valid_list'])
 
-    output = [(a, b, c) for a, b, c in zip(td._results['t1w_valid_list'], target_zooms, target_shapes)]
-
+    output = [(a, (b, c, d)) for a, b, c, d in zip(td._results['t1w_valid_list'],
+                                                target_zooms, target_shapes, out_report)]
     return output
 
 def t1ConformTask(img):
     c = Conform()
 
     c.inputs.in_file = img[0]
-    c.inputs.target_zooms = img[1]
-    c.inputs.target_shape = img[2]
+    c.inputs.target_zooms = img[1][0]
+    c.inputs.target_shape = img[1][1]
    
     interface_dir = workdir + '/Conform'
 
@@ -73,6 +75,27 @@ def t1ConformTask(img):
 
     return (c._results['out_file'], c._results['transform'])
 
+#def n4BiasFieldCorrectionTask(img):
+
+
+def outputIdentityTaskSingleT1(part):
+    part = list(part)
+
+    out_id = createOutputInterfaceObj()
+
+    out_id.inputs.template_transforms = [pkgr('fmriprep', 'data/itkIdentityTransform.txt')]
+    out_id.inputs.t1_template = part[0][0]
+    out_id.inputs.t1w_valid_list = [i[0] for i in part]
+    out_id.inputs.out_report = part[0][1][1][2]
+
+    out = out_id.run()
+
+    return (out.outputs.t1w_valid_list, (out.outputs.t1_template, 
+            out.outputs.template_transforms, out.outputs.out_report))
+
+def createOutputInterfaceObj():
+    return niu.IdentityInterface(fields=['t1_template', 't1w_valid_list', 'template_transforms', 'out_report'])
+
 def create_spark_context(workflow_name):
     conf = SparkConf().setAppName(workflow_name)
     return SparkContext.getOrCreate(conf=conf)
@@ -84,29 +107,30 @@ def execute_wf(workflow_name, workdir):
 
     sc = create_spark_context(workflow_name)
 
-    in_t1_rdd = sc.parallelize(input_t1w) \
+    temp_dim_rdd = sc.parallelize(input_t1w) \
                     .coalesce(1) \
-                    .mapPartitions(runTemplateDimensions) \
-                    .repartition(8) \
+                    .mapPartitions(templateDimensionsTask) \
+                    .cache()
+
+    t1_conform_rdd = temp_dim_rdd.repartition(8) \
                     .map(t1ConformTask) \
                     .cache()
 
-    out_identity = niu.IdentityInterface(fields=['t1_template', 't1w_valid_list', 'template_transforms', 'out_report'])
-
-
+    out_rdd = t1_conform_rdd.coalesce(1) \
+                            .join(temp_dim_rdd) \
+                            .cache()
     if num_t1w == 1:
-        def _get_first(in_list):
-            if isinstance(in_list, (list, tuple)):
-                return in_list[0]
-            return in_list
+        out_rdd = out_rdd.coalesce(1) \
+                         .mapPartitions(
+                                 outputIdentityTaskSingleT1
+                                 ) \
+                         .collect()
+        return out_rdd
 
-        
 
 
 
-    print(in_t1_rdd.collect())
+    #print(t1_conform_rdd.collect())
 
-    # t1_template_dimensions = pe.Node(TemplateDimensions(), name='t1_template_dimensions')
-    # t1_conform = pe.MapNode(Conform(), iterfield='in_file', name='t1_conform')
     
 execute_wf(workflow_name, workdir)
