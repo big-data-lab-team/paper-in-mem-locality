1da548ea816e0109965debf32c1023517a4497c9
valhayot <valeriehayot@gmail.com>
2018-10-20 17:19:24 -0400

    added test case

---
+++ tests/test_inc.py
@@ -0,0 +1,30 @@
+import pytest
+import subprocess
+import hashlib
+
+
+def test_increment():
+
+    p = subprocess.Popen(['python', 'spark_inc.py', 'sample_data', 'inc_out',
+                          '1'])
+    p.communicate()
+
+    h_prog_1 = hashlib.md5(open('inc_out/inc-dummy_1.nii', 'rb').read()) \
+                      .hexdigest()
+    h_exp_1 = hashlib.md5(open('tests/test_outputs/output_1_1.nii', 'rb')
+                          .read()) \
+                     .hexdigest()
+
+    assert h_prog_1 == h_exp_1
+
+    p = subprocess.Popen(['python', 'spark_inc.py', 'sample_data', 'inc_out',
+                          '10'])
+    p.communicate()
+
+    h_prog_10 = hashlib.md5(open('inc_out/inc-dummy_1.nii', 'rb').read()) \
+                       .hexdigest()
+    h_exp_10 = hashlib.md5(open('tests/test_outputs/output_1_10.nii', 'rb')
+                           .read()) \
+                      .hexdigest()
+
+    assert h_prog_10 == h_exp_10
