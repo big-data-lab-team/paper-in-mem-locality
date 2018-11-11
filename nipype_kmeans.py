4111d2c9f094eb5b6b5d7aa20450d0e2cbbb47e2
valhayot <valeriehayot@gmail.com>
2018-11-10 23:47:21 -0500

    nearly functioning kmeans

--- nipype_kmeans.py
+++ nipype_kmeans.py
@@ -1,156 +1,154 @@
 from nipype import Workflow, MapNode, Node, Function
-from io import BytesIO
-import argparse, os
+from numpy import iinfo
+from random import seed, sample
+import argparse
+import os
 import glob
 
-def get_nearest_centroid(chunk, cent_0, cent_1, cent_2, cent_3):
+
+def get_nearest_centroid(img, centroids):
     import nibabel as nib
-    import numpy as np
-    import os
+    import pickle
+    from os import path as op
 
-    im = nib.load(chunk)
-    data = im.get_data()
+    data = nib.load(img).get_fdata().flatten()
 
-    centroids = [cent_0, cent_1, cent_2, cent_3]
+    assignments = {}
 
-    asgmt_0 = []
-    asgmt_1 = []
-    asgmt_2 = []
-    asgmt_3 = []
+    for vox in data:
+        distance = None
+        nearest_c = None
 
-    file_0 = 'asgmt_0.txt'
-    file_1 = 'asgmt_1.txt'
-    file_2 = 'asgmt_2.txt'
-    file_3 = 'asgmt_3.txt'
+        for c in centroids:
+            c_dist = abs(vox - c[1])
 
-    for vox in data.flatten():
-        distance = [abs(c - vox) for c in centroids]
-        c_idx = np.argmin(distance)
-        
-        if c_idx == 0:
-            asgmt_0.append(vox)
-        elif c_idx == 1:
-            asgmt_1.append(vox)
-        elif c_idx == 2:
-            asgmt_2.append(vox)
+            if distance is None or c_dist < distance:
+                distance = c_dist
+                nearest_c = c[0]
+
+        if nearest_c not in assignments:
+            assignments[nearest_c] = [vox]
         else:
-            asgmt_3.append(vox)
-    #print(asgmt_0, asgmt_1, asgmt_2, asgmt_3)
-    np.savetxt(file_0, asgmt_0, header=str(cent_0))
-    np.savetxt(file_1, asgmt_1, header=str(cent_1))
-    np.savetxt(file_2, asgmt_2, header=str(cent_2))
-    np.savetxt(file_3, asgmt_3, header=str(cent_3))
+            assignments[nearest_c].append(vox)
+
+    outfiles = []
+
+    try:
+        for k, v in assignments.items():
+            out_name = 'centroid-{}.out'.format(k)
+            with open(out_name, 'ab+') as f:
+                pickle.dump(v, f)
+
+            outfiles.append((k, op.abspath(out_name)))
+
+    except Exception as e:
+        for k, v in assignments.iteritems():
+            out_name = 'centroid-{}.out'.format(k)
+
+            with open('centroid-{}.out'.format(k), 'ab+') as f:
+                pickle.dump(v, f)
+
+            outfiles.append((k, op.abspath(out_name)))
+
+    return outfiles
 
 
-    return (os.path.abspath(file_0), os.path.abspath(file_1),
-            os.path.abspath(file_2), os.path.abspath(file_3))
+def update_centroids(centroid, assignments):
+    import pickle
 
-def update_centroids(assignment_files):
-    import numpy as np
-    import os
+    a_files = [t[1] for l in assignments for t in l if t[0] == centroid[0]]
 
-    v_sum = 0
-    length = 0
-    cent = None
+    sum_elements = 0
+    num_elements = 0
 
-    with open(assignment_files[0], 'r') as f:
-        cent = float(f.readline().strip('#').strip())
+    for fn in a_files:
+        with open(fn, 'rb') as f:
+            elements = pickle.load(f)
+            sum_elements += sum([float(i) for i in elements])
+            num_elements += len(elements)
 
-    for f in assignment_files:
-        v = np.loadtxt(f)
-        v_sum += v.sum()
-        length += v.size
+    if sum_elements == num_elements == 0:
+        return centroid
+    else:
+        return (centroid[0], sum_elements/num_elements)
 
-    fn = 'ucent.txt'
-    # keep centroid the same if no elements are assigned
-    if sum == 0:
-        with open(fn, 'w+') as f:
-            f.write(str(cent))
-        return os.path.abspath(fn)
-
-    with open(fn, 'w+') as f:
-        f.write(str(v_sum/length))
-
-    return os.path.abspath(fn)
 
 def main():
-    parser = argparse.ArgumentParser(description="BigBrain K-means")
-
-    parser.add_argument('bb_dir',type=str, help='The folder containing BigBrain NIfTI images (local fs only)')
-    #parser.add_argument('output_dir', type=str, help='the folder to save binarized images to (local fs only)')
-    parser.add_argument('iters', type=int, help='The number of kmeans iterations')
+    parser = argparse.ArgumentParser(description='BigBrain K-means')
+    parser.add_argument('bb_dir', type=str, help='The folder containing '
+                        'BigBrain NIfTI images (local fs only)')
+    parser.add_argument('output_dir', type=str, help='the folder to save '
+                        'the final centroids to (local fs only)')
+    parser.add_argument('iters', type=int, help='The number of iterations')
 
     args = parser.parse_args()
 
-    wf = Workflow('km_bb')
-    wf.base_dir = os.getcwd()
+    output_dir = os.path.abspath(args.output_dir)
 
-    #output_dir = os.path.abspath(args.output_dir)
-    #os.makedirs(output_dir, exist_ok=True)
+    try:
+        os.makedirs(output_dir)
+    except Exception as e:
+        pass
 
     # get all files in directory
     bb_files = glob.glob(os.path.join(os.path.abspath(args.bb_dir), '*'))
-    centroids = [50314.747730447438, 5.4095965052460562, 29218.970083958127,
-                                                         60767.571375735897]
+    seed(2)
+    dtype = iinfo('uint16')
 
-    #loaded_data = MapNode(Function(input_names=))
+    centroids = sample(range(dtype.min, dtype.max), 5)
 
-    func_asgmt = Function(input_names=['chunk', 'cent_0', 'cent_1', 
-                                     'cent_2', 'cent_3'],
-                                     output_names=['asgmt_0', 'asgmt_1', 'asgmt_2',
-                                     'asgmt_3'],
-                                     function=get_nearest_centroid)
+    centroids = list(zip(range(0, len(centroids)), centroids))
 
-    func_updatec = Function(input_names=['assignment_files'],
-                                     output_names=['up_cent'],
-                                     function=update_centroids)
+    c_changed = True
 
-    up_centroid0 = None
-    up_centroid1 = None
-    up_centroid2 = None
-    up_centroid3 = None
+    idx = 0
+    while c_changed and idx < args.iters:
+        wf = Workflow('km_bb{}'.format(idx))
 
-    for i in range(args.iters):
+        gc = MapNode(Function(input_names=['img', 'centroids'],
+                              output_names=['assignment_files'],
+                              function=get_nearest_centroid),
+                     name='gc_{}'.format(idx),
+                     iterfield=['img'])
 
-        asgmt_name = 'assignments{}'.format(i)
-        updated0_name = 'upcentroids0_{}'.format(i)
-        updated1_name = 'upcentroids1_{}'.format(i)
-        updated2_name = 'upcentroids2_{}'.format(i)
-        updated3_name = 'upcentroids3_{}'.format(i)
+        gc.inputs.img = bb_files
+        gc.inputs.centroids = centroids
 
-        assignments = MapNode(func_asgmt, iterfield=['chunk'],
-                                name='assignments{}'.format(i))
+        wf.add_nodes([gc])
 
-        assignments.inputs.chunk = bb_files
+        uc = MapNode(Function(input_names=['centroid', 'assignments'],
+                              output_names=['updated_centroids'],
+                              function=update_centroids),
+                     name='uc_{}'.format(idx),
+                     iterfield=['centroid'])
 
-        if i == 0:
-            assignments.inputs.cent_0 = centroids[0]
-            assignments.inputs.cent_1 = centroids[1]
-            assignments.inputs.cent_2 = centroids[2]
-            assignments.inputs.cent_3 = centroids[3]
-         
-            wf.add_nodes([assignments])
+        uc.inputs.centroid = centroids
 
+        wf.connect([(gc, uc, [('assignment_files', 'assignments')])])
+
+        wf_out = wf.run(plugin='MultiProc')
+
+        # Convert to dictionary to more easily extract results
+        node_names = [i.name for i in wf_out.nodes()]
+        result_dict = dict(zip(node_names, wf_out.nodes()))
+
+        new_centroids = (result_dict['uc_{}'.format(idx)].result
+                                                         .outputs
+                                                         .updated_centroids)
+
+        old_centroids = set(centroids)
+        diff = [x for x in new_centroids if x not in old_centroids]
+        c_changed = bool(diff)
+        centroids = new_centroids
+
+        c_vals = [i[1] for i in centroids]
+        idx += 1
+
+        if c_changed and idx < args.iters:
+            print("it", idx, c_vals)
         else:
-            wf.connect([(up_centroid0, assignments, [('up_cent','cent_0')]),
-                        (up_centroid1, assignments, [('up_cent','cent_1')]),
-                        (up_centroid2, assignments, [('up_cent','cent_2')]),
-                        (up_centroid3, assignments, [('up_cent','cent_3')])
-                      ]) 
+            print("***FINAL CENTROIDS***:", c_vals)
 
-        up_centroid0 = Node(func_updatec, name=updated0_name)
-        up_centroid1 = Node(func_updatec, name=updated1_name)
-        up_centroid2 = Node(func_updatec, name=updated2_name)
-        up_centroid3 = Node(func_updatec, name=updated3_name)
-
-        wf.connect([(assignments, up_centroid0, [('asgmt_0','assignment_files')]),
-                   (assignments, up_centroid1, [('asgmt_1','assignment_files')]),
-                   (assignments, up_centroid2, [('asgmt_2','assignment_files')]),
-                   (assignments, up_centroid3, [('asgmt_3','assignment_files')])
-                  ]) 
-
-
-    wf.run(plugin='MultiProc')
 
 if __name__ == '__main__':
     main()
