444bba46aba092162eec65c175ac98c72fe66676
valhayot <valeriehayot@gmail.com>
2018-11-15 16:57:21 -0600

    bug fix

--- benchmark_scripts/nipype_sbatch.sh
+++ benchmark_scripts/nipype_sbatch.sh
@@ -3,7 +3,7 @@
 source /mnt/lustrefs/spark/env/exp0/bin/activate
 export PATH=/mnt/lustrefs/spark/SOEN691-project:$PATH
 
-echo "{\"n_procs:\" ${SLURM_CPUS_PER_TASK}}" > /mnt/lustrefs/spark/multiproc_conf.json
+echo "{\"n_procs\": ${SLURM_CPUS_PER_TASK}}" > /mnt/lustrefs/spark/multiproc_conf.json
 
 $npscript --plugin MultiProc --plugin_args /mnt/lustrefs/spark/multiproc_conf.json
 
