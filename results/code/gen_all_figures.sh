5b6c6ef31b3380f359236f32c76d2df127d1138e
valhayot <valeriehayot@gmail.com>
2018-12-11 23:31:23 -0500

    modified script to be executed from main directory

--- results/code/gen_all_figures.sh
+++ results/code/gen_all_figures.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-python get_makespan.py ../benchmarks incrementation_makespan.out
+python results/code/get_makespan.py results/benchmarks results/code/incrementation_makespan.out
 
-python d_c_graph.py ../benchmarks incrementation_makespan.out ../figures/incrementation.pdf
-python gen_figures.py 1 incrementation_makespan.out ../figures/iterations.pdf
-python gen_figures.py 2 incrementation_makespan.out ../figures/numchunks.pdf
-python gen_figures.py 3 incrementation_makespan.out ../figures/datasize.pdf
-python gen_figures.py 4 incrementation_makespan.out ../figures/cputime.pdf
+python results/code/d_c_graph.py results/benchmarks results/code/incrementation_makespan.out results/figures/incrementation.pdf
+python results/code/gen_figures.py 1 results/code/incrementation_makespan.out results/figures/iterations.pdf
+python results/code/gen_figures.py 2 results/code/incrementation_makespan.out results/figures/numchunks.pdf
+python results/code/gen_figures.py 3 results/code/incrementation_makespan.out results/figures/datasize.pdf
+python results/code/gen_figures.py 4 results/code/incrementation_makespan.out results/figures/cputime.pdf
