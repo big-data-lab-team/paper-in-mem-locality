4f39c71b653bf895122ba77da0941dab215c97a0
valhayot <valeriehayot@gmail.com>
2019-03-28 22:53:31 -0400

    final fixes

--- results/code/gen_all_figures.sh
+++ results/code/gen_all_figures.sh
@@ -3,11 +3,7 @@
 python results/code/get_makespan.py results/benchmarks results/code/incrementation_makespan.out
 
 python results/code/d_c_graph.py results/benchmarks results/code/incrementation_makespan.out results/figures/incrementation.pdf
-python results/code/gen_figures.py 1 results/code/incrementation_makespan.out -f "spark" results/figures/s-iterations.pdf
-python results/code/gen_figures.py 2 results/code/incrementation_makespan.out -f "spark" results/figures/s-numchunks.pdf
-python results/code/gen_figures.py 3 results/code/incrementation_makespan.out -f "spark" results/figures/s-datasize.pdf
-python results/code/gen_figures.py 4 results/code/incrementation_makespan.out -f "spark" results/figures/s-cputime.pdf
-python results/code/gen_figures.py 1 results/code/incrementation_makespan.out -f "nipype" results/figures/n-iterations.pdf
-python results/code/gen_figures.py 2 results/code/incrementation_makespan.out -f "nipype" results/figures/n-numchunks.pdf
-python results/code/gen_figures.py 3 results/code/incrementation_makespan.out -f "nipype" results/figures/n-datasize.pdf
-python results/code/gen_figures.py 4 results/code/incrementation_makespan.out -f "nipype" results/figures/n-cputime.pdf
+python results/code/gen_figures.py 1 results/code/incrementation_makespan.out results/figures/iterations.pdf
+python results/code/gen_figures.py 2 results/code/incrementation_makespan.out results/figures/numchunks.pdf
+python results/code/gen_figures.py 3 results/code/incrementation_makespan.out results/figures/datasize.pdf
+python results/code/gen_figures.py 4 results/code/incrementation_makespan.out results/figures/cputime.pdf
